4a14d69284dc9bcd090fa51d42eb583003ce259c
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-04-09 15:33:39 -0400

    Fixes!

--- handlers/lambdaHandler.ts
+++ handlers/lambdaHandler.ts
@@ -1,36 +1,76 @@
-import { Handler } from 'aws-lambda';
-import { S3, Endpoint } from 'aws-sdk';
+import { Handler } from "aws-lambda";
+import { S3, Endpoint } from "aws-sdk";
 
+const lambdaHandler: Handler = async (
+  event: any,
+  context,
+  callback: () => void
+) => {
+  if (!event?.pathParameters?.id) {
+    const response = {
+      statusCode: 400,
+      statusText: "No id provided",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ error: "no id provided" }),
+    };
+  }
+  const s3 = process.env.IS_OFFLINE
+    ? new S3({
+      s3ForcePathStyle: true,
+      endpoint: new Endpoint(process.env.s3Endpoint),
+      accessKeyId: process.env.s3AccessKey,
+      secretAccessKey: process.env.s3SecretAccessKey,
+    })
+    : new S3();
 
-const lambdaHandler: Handler = async (event: any, context, callback: () => void) => {
+  try {
+    let s3object = await s3
+      .getObject({ Bucket: process.env.bucket, Key: event.pathParameters.id })
+      .promise();
+    if (s3object?.Body) {
+      let time = JSON.parse(s3object.Body.toString("utf-8"));
 
-  const s3 = process.env.IS_OFFLINE ? new S3({
-    s3ForcePathStyle: true,
-    endpoint: new Endpoint(process.env.s3Endpoint),
-    accessKeyId: process.env.s3AccessKey,
-    secretAccessKey: process.env.s3SecretAccessKey,
-  }) :
-    new S3();
+      const response = {
+        statusCode: 200,
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ greeting: "last said Hi at " + time.time }),
+      };
+      return response;
+    }
+  } catch (err) { }
 
-  const s3Params = {
-    Bucket: process.env.bucket,
-    Key: Date.now().toString(),
-    Body: {
-      greeting: "Hi"
-    },
-    ContentType: 'application/json',
-    ACL: "public-read"
-  };
+  const time = Date.now().toString()
+  try {
+    const s3Params = {
+      Bucket: process.env.bucket,
+      Key: event.pathParameters.id,
+      Body: JSON.stringify({
+        time: time,
+      }),
+      ContentType: "application/json",
+      ACL: "private",
+    };
 
-  await s3.putObject(s3Params).promise();
+    await s3.putObject(s3Params).promise();
 
-  const response = {
-    statusCode: 200,
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({ 'greeting': 'stored a greeting in S3 at ' + Date.now().toString() })
-  };
+    const response = {
+      statusCode: 200,
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        greeting: "stored a greeting in S3 at " + Date.now().toString(),
+      }),
+    }
+    return response;
+  }
+  catch (err) {
+    const response = {
+      statusCode: 400,
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ error: "something went badly wrong" }),
+    };
+    return response;
+  }
 
-  return response;
-}
+};
 
 export default lambdaHandler;
